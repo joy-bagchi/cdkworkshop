f60c7d295aa2493a36fc111c4fde835e41d06fe9
Jay Bagchi <joy.bagchi@gmail.com>
2021-01-12 00:04:10 -0500

    Final Commit for CDK Workshop

--- cdkworkshop/cdkworkshop_stack.py
+++ cdkworkshop/cdkworkshop_stack.py
@@ -1,24 +1,36 @@
 from aws_cdk import (
     aws_iam as iam,
-    aws_sqs as sqs,
-    aws_sns as sns,
-    aws_sns_subscriptions as subs,
+    aws_lambda as _lambda,
+    aws_apigateway as apigw,
     core
 )
 
+from hitcounter import HitCounter
+from cdk_dynamo_table_viewer import TableViewer
 
 class CdkworkshopStack(core.Stack):
 
     def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        queue = sqs.Queue(
-            self, "CdkworkshopQueue",
-            visibility_timeout=core.Duration.seconds(300),
+        my_lambda = _lambda.Function(self, 'HelloHandler',
+                                     runtime=_lambda.Runtime.PYTHON_3_8,
+                                     code=_lambda.Code.asset('lambda'),
+                                     handler='hello.handler'
+                                     )
+
+        hello_with_counter = HitCounter(
+            self, 'HelloHitCounter',
+            downstream=my_lambda
         )
 
-        topic = sns.Topic(
-            self, "CdkworkshopTopic"
+        apigw.LambdaRestApi(
+            self, 'Endpoint',
+            handler=hello_with_counter.handler,
         )
 
-        topic.add_subscription(subs.SqsSubscription(queue))
+        TableViewer(
+            self, 'ViewHitCounter',
+            title='Hello Hits',
+            table=hello_with_counter.table
+        )
